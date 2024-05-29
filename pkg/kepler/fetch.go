5d8b6236e35b3cd8d0b6d4721f237754a01f8f88
Gabi Beyer <gabi@re-cinq.com>
2024-05-29 15:28:25 +0200

    refactor: clean up code duplication

--- pkg/kepler/fetch.go
+++ pkg/kepler/fetch.go
@@ -29,13 +29,13 @@ func (k *KeplerSource) Fetch(ctx context.Context) ([]*v1.Instance, error) {
 	k.v1API = prometheus.NewAPI(*k.client)
 
 	// Get CPU energy consumption for each container
-	err := k.cpuMetrics(ctx, fmt.Sprintf(query, "core", k.cfg.Interval.String()))
+	err := k.getMetrics(ctx, fmt.Sprintf(query, "core", k.cfg.Interval.String()), v1.CPU)
 	if err != nil {
 		return nil, err
 	}
 
 	// Get Memory energy consumption for each container
-	err = k.memMetrics(ctx, fmt.Sprintf(query, "dram", k.cfg.Interval.String()))
+	err = k.getMetrics(ctx, fmt.Sprintf(query, "dram", k.cfg.Interval.String()), v1.Memory)
 	if err != nil {
 		return nil, err
 	}
@@ -50,12 +50,7 @@ func (k *KeplerSource) Fetch(ctx context.Context) ([]*v1.Instance, error) {
 	return instances, nil
 }
 
-func (k *KeplerSource) cpuMetrics(ctx context.Context, query string) error {
-	//r := prometheus.Range{
-	//	Start: time.Now().Add(-k.cfg.Interval),
-	//	End:   time.Now(),
-	//	Step:  time.Minute,
-	//}
+func (k *KeplerSource) getMetrics(ctx context.Context, query string, rt v1.ResourceType) error {
 	result, warnings, err := k.v1API.Query(ctx, query, time.Now())
 	if err != nil {
 		k.logger.Error("kelper source: error querying prometheus", "error", err)
@@ -73,36 +68,39 @@ func (k *KeplerSource) cpuMetrics(ctx context.Context, query string) error {
 	}
 
 	for _, sample := range vector {
-		// store labels to be added to metric and construct the instance
+		// store metric labels
 		labels := make(map[string]string)
 		for k, v := range sample.Metric {
 			labels[string(k)] = string(v)
 		}
-
-		if sample.Value == 0 {
-			k.logger.Debug("kepler source: CPU energy consumption is 0", "containerID", labels["container_id"], "container", labels["container_name"])
-			continue
-		}
-
-		// Convert the Joules energy consumption collected from kepler
-		// into KwH
-		// 1 Joule = 1 Watt second, so divide Joules by the interval of time in seconds
-		// and divide by 3600 to get watt hours, and divide by 1000 to get kilowatt hours.
-		ekWh := float64(sample.Value) / k.cfg.Interval.Seconds() / 3600 / 1000
-
-		// Create the CPU metric with the energy consumtion
-		// collected from the query
-		m := v1.NewMetric(v1.CPU.String())
-		m.ResourceType = v1.CPU
-		m.Energy = ekWh
-		m.Labels = labels
 
 		// since container_id is unique, we can use it as the instanceMap key
 		// and ID for the instance
 		id := labels["container_id"]
 		name := labels["container_name"]
 
-		k.logger.Debug("kepler source: CPU energy consumption kWh", "containerID", id, "container", name, "energy", m.Energy)
+		if sample.Value == 0 {
+			k.logger.Debug("kepler source: energy consumption is 0",
+				"metric", rt,
+				"containerID", id,
+				"container", name,
+			)
+			continue
+		}
+
+		// Create the metric with the energy consumption
+		// collected from the query
+		m := v1.NewMetric(rt.String())
+		m.ResourceType = rt
+		m.Energy = convertJoulesTokWh(float64(sample.Value), k.cfg.Interval.Seconds())
+		m.Labels = labels
+
+		k.logger.Debug("kepler source: energy consumption kWh",
+			"metric", rt,
+			"containerID", id,
+			"container", name,
+			"energy", m.Energy,
+		)
 
 		// if the instance already exists in the instancesMap
 		// upsert the metric and continue looping
@@ -111,130 +109,66 @@ func (k *KeplerSource) cpuMetrics(ctx context.Context, query string) error {
 			continue
 		}
 
-		// if the instance doesn't already exist, create one
-		// from the labels provided
-		region, exists := labels["region"]
-		if !exists {
-			region, err = getRegionFromInstance(labels["instance"])
-			if err != nil {
-				k.logger.Error("kepler source: error getting region from instance", "error", err)
-				continue
-			}
+		// otherwise create a new instance from label data
+		instance, err := k.createInstance(labels, id, name)
+		if err != nil {
+			k.logger.Error("kepler source: error creating instance", "error", err)
+			continue
 		}
 
-		p, ok := v1.Providers[k.cfg.Provider]
-		if !ok {
-			return fmt.Errorf("provider %s not found", k.cfg.Provider)
-		}
-
-		instance := &v1.Instance{
-			ID:       id,
-			Provider: p,
-			Service:  "kepler",
-			Name:     name,
-			Region:   region,
-			Status:   v1.InstanceRunning,
-			Labels:   labels,
-		}
-
+		// upsert the metric to the instance
 		instance.Metrics.Upsert(m)
+		k.logger.Debug("kepler source: upserted metric to instance",
+			"metric", rt,
+			"containerID", id,
+			"container", name,
+		)
+
+		// add the instance to the instancesMap
 		k.instancesMap[id] = instance
 	}
 
 	return nil
 }
 
-func (k *KeplerSource) memMetrics(ctx context.Context, query string) error {
-	//r := prometheus.Range{
-	//	Start: time.Now().Add(-k.cfg.Interval),
-	//	End:   time.Now(),
-	//	Step:  time.Minute,
-	//}
-	result, warnings, err := k.v1API.Query(ctx, query, time.Now())
-	if err != nil {
-		k.logger.Error("kelper source: error querying prometheus", "error", err)
-		return err
+// createInstance creates an instance type that metric data is collected for by
+// parsing the label information
+func (k *KeplerSource) createInstance(labels map[string]string, id, name string) (*v1.Instance, error) {
+	var err error
+
+	// see if the region exists as a label, and if not try and
+	// extract it from the instance name
+	region, exists := labels["region"]
+	if !exists {
+		region, err = getRegionFromInstance(labels["instance"])
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	if len(warnings) > 0 {
-		k.logger.Warn("kepler source: prometheus query warnings", "warnings", warnings)
-	}
-
-	// Check if the result is a model.Vector
-	vector, ok := result.(model.Vector)
+	// get provider name from authorized providers
+	p, ok := v1.Providers[k.cfg.Provider]
 	if !ok {
-		return fmt.Errorf("expected a model.Vector but got %T", result)
+		return nil, fmt.Errorf("provider %s not found", k.cfg.Provider)
 	}
 
-	for _, sample := range vector {
-		labels := make(map[string]string)
-		for k, v := range sample.Metric {
-			labels[string(k)] = string(v)
-		}
+	return &v1.Instance{
+		ID:       id,
+		Provider: p,
+		Service:  "kepler",
+		Name:     name,
+		Region:   region,
+		Status:   v1.InstanceRunning,
+		Labels:   labels,
+	}, nil
+}
 
-		if sample.Value == 0 {
-			k.logger.Debug("kepler source: memory energy consumption is 0", "containerID", labels["container_id"], "container", labels["container_name"])
-			continue
-		}
-
-		// Convert the Joules energy consumption collected from kepler
-		// into KwH
-		// 1 Joule = 1 Watt second, so divide Joules by the interval of time in seconds
-		// and divide by 3600 to get watt hours, and divide by 1000 to get kilowatt hours.
-		ekWh := float64(sample.Value) / k.cfg.Interval.Seconds() / 3600 / 1000
-
-		// Create the memory metric with the energy consumtion
-		// collected from the query
-		m := v1.NewMetric(v1.Memory.String())
-		m.ResourceType = v1.Memory
-		m.Energy = ekWh
-		m.Labels = labels
-
-		// since container_id is unique, we can use it as the instanceMap key
-		// and ID for the instance
-		id := labels["container_id"]
-		name := labels["container_name"]
-
-		k.logger.Debug("kepler source: memory energy consumption kWh", "containerID", id, "container", name, "energy", m.Energy)
-
-		// if the instance already exists in the instancesMap
-		// upsert the metric and continue looping
-		if instance, exists := k.instancesMap[id]; exists {
-			instance.Metrics.Upsert(m)
-			continue
-		}
-
-		// if the instance doesn't yet exist, create it from
-		// the labels
-		region, exists := labels["region"]
-		if !exists {
-			region, err = getRegionFromInstance(labels["instance"])
-			if err != nil {
-				k.logger.Error("kepler source: error getting region from instance", "error", err)
-				continue
-			}
-		}
-
-		p, ok := v1.Providers[k.cfg.Provider]
-		if !ok {
-			return fmt.Errorf("provider %s not found", k.cfg.Provider)
-		}
-
-		instance := &v1.Instance{
-			ID:       id,
-			Provider: p,
-			Service:  "kepler",
-			Name:     name,
-			Status:   v1.InstanceRunning,
-			Region:   region,
-			Labels:   labels,
-		}
-
-		instance.Metrics.Upsert(m)
-		k.instancesMap[id] = instance
-	}
-
-	return nil
+// convertJoulesTokWh converts the energy consumption collected from kepler
+// from Joules to Kilowatt hours.
+// 1 Joule = 1 Watt second, so divide Joules by the interval of time in seconds
+// and divide by 3600 to get watt hours, and divide by 1000 to get kilowatt hours.
+func convertJoulesTokWh(j, s float64) float64 {
+	return j / s / 3600 / 1000
 }
 
 // getRegionFromInstance takes a prometheus metrics label of
