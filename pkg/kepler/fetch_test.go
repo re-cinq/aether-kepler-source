73fd6202146e3919569911a90a299123b6a47fec
Gabi Beyer <gabi@re-cinq.com>
2024-05-29 15:52:40 +0200

    refactor: Add validation to conversion func
    Check that energy input isn't 0 within the conversion
    functionality and add unit tests.

--- pkg/kepler/fetch_test.go
+++ pkg/kepler/fetch_test.go
@@ -6,6 +6,62 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestConvertJoulesTokWh(t *testing.T) {
+	type testcase struct {
+		name            string
+		joules          float64
+		intervalSeconds float64
+		kWh             float64
+		err             string
+	}
+
+	for _, test := range []*testcase{
+		func() *testcase {
+			return &testcase{
+				name:            "valid conversion",
+				joules:          1000,
+				intervalSeconds: 300, // 5 minutes
+				kWh:             9.259259259259259e-07,
+			}
+		}(),
+		func() *testcase {
+			return &testcase{
+				name:            "valid conversion 1 minute",
+				joules:          1000,
+				intervalSeconds: 60, // 1 minute
+				kWh:             4.62962962962963e-06,
+			}
+		}(),
+		func() *testcase {
+			return &testcase{
+				name:            "valid conversion 372 J",
+				joules:          372,
+				intervalSeconds: 300, // 5 minutes
+				kWh:             3.4444444444444444e-07,
+			}
+		}(),
+		func() *testcase {
+			return &testcase{
+				name:            "error invalid 0 J",
+				joules:          0,
+				intervalSeconds: 300, // 5 minutes
+				kWh:             0,
+				err:             "energy consumption is 0",
+			}
+		}(),
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			actualKWh, err := convertJoulesTokWh(test.joules, test.intervalSeconds)
+			assert.Equalf(t, test.kWh, actualKWh, "Result should be: %v, got: %v", test.kWh, actualKWh)
+			if test.err != "" {
+				assert.Errorf(t, err, test.err)
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
 func TestGetRegionFromInstance(t *testing.T) {
 	type testcase struct {
 		name     string
